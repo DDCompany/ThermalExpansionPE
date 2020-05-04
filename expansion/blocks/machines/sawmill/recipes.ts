2b49f0432b112e19ad4a41a0b78b705fe953291b
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-05-04 21:50:11 +0300

    Rework sawmill recipes

--- expansion/blocks/machines/sawmill/recipes.ts
+++ expansion/blocks/machines/sawmill/recipes.ts
@@ -1,162 +1,181 @@
+//log -> planks
 sawmillManager.add({
-    input: {id: 17, data: 0},
-    result: {id: 5, data: 0, count: 6},
+    input: {id: VanillaBlockID.log},
+    result: {id: VanillaBlockID.planks, data: 0, count: 6},
     second: {id: ItemID.dustSaw, data: 0, count: 1}
 });
 
 sawmillManager.add({
-    input: {id: 17, data: 1},
-    result: {id: 5, data: 1, count: 6},
+    input: {id: VanillaBlockID.log, data: 1},
+    result: {id: VanillaBlockID.planks, data: 1, count: 6},
     second: {id: ItemID.dustSaw, data: 0, count: 1}
 });
 sawmillManager.add({
-    input: {id: 17, data: 2},
-    result: {id: 5, data: 2, count: 6},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
-});
-
-sawmillManager.add({
-    input: {id: 17, data: 3},
-    result: {id: 5, data: 3, count: 6},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
-});
-sawmillManager.add({
-    input: {id: 162, data: 0},
-    result: {id: 5, data: 4, count: 6},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
-});
-sawmillManager.add({
-    input: {id: 162, data: 1},
-    result: {id: 5, data: 5, count: 6},
+    input: {id: VanillaBlockID.log, data: 2},
+    result: {id: VanillaBlockID.planks, data: 2, count: 6},
     second: {id: ItemID.dustSaw, data: 0, count: 1}
 });
 
 sawmillManager.add({
-    input: {id: 25, data: 0},
-    result: {id: 5, data: 0, count: 8},
-    second: {id: 331, data: 0, count: 1, chance: 0.25}
-});
-sawmillManager.add({
-    input: {id: 355, data: 0},
-    result: {id: 5, data: 0, count: 2},
-    second: {id: 35, data: 0, count: 2}
-});
-sawmillManager.add({
-    input: {id: 47, data: 0},
-    result: {id: 5, data: 0, count: 3},
-    second: {id: 340, data: 0, count: 3, chance: 0.25}
-});
-sawmillManager.add({
-    input: {id: 58, data: 0},
-    result: {id: 5, data: 0, count: 2},
+    input: {id: VanillaBlockID.log, data: 3},
+    result: {id: VanillaBlockID.planks, data: 3, count: 6},
     second: {id: ItemID.dustSaw, data: 0, count: 1}
 });
 sawmillManager.add({
-    input: {id: 323, data: 0},
-    result: {id: 5, data: 0, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-sawmillManager.add({
-    input: {id: 324, data: 0},
-    result: {id: 5, data: 0, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-sawmillManager.add({
-    input: {id: 427, data: 0},
-    result: {id: 5, data: 1, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-sawmillManager.add({
-    input: {id: 428, data: 0},
-    result: {id: 5, data: 2, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-sawmillManager.add({
-    input: {id: 429, data: 0},
-    result: {id: 5, data: 3, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-sawmillManager.add({
-    input: {id: 430, data: 0},
-    result: {id: 5, data: 4, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-sawmillManager.add({
-    input: {id: 431, data: 0},
-    result: {id: 5, data: 5, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-
-sawmillManager.add({
-    input: {id: 72, data: 0},
-    result: {id: 5, data: 0, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.5}
-});
-sawmillManager.add({
-    input: {id: 96, data: 0},
-    result: {id: 5, data: 0, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.75}
-});
-sawmillManager.add({
-    input: {id: 333, data: 0},
-    result: {id: 5, data: 0, count: 2},
+    input: {id: VanillaBlockID.log2},
+    result: {id: VanillaBlockID.planks, data: 4, count: 6},
     second: {id: ItemID.dustSaw, data: 0, count: 1}
 });
 sawmillManager.add({
-    input: {id: 333, data: 1},
-    result: {id: 5, data: 1, count: 2},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
-});
-sawmillManager.add({
-    input: {id: 333, data: 2},
-    result: {id: 5, data: 2, count: 2},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
-});
-sawmillManager.add({
-    input: {id: 333, data: 3},
-    result: {id: 5, data: 3, count: 2},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
-});
-sawmillManager.add({
-    input: {id: 333, data: 4},
-    result: {id: 5, data: 4, count: 2},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
-});
-sawmillManager.add({
-    input: {id: 269, data: 0},
-    result: {id: 5, data: 0, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.75}
-});
-sawmillManager.add({
-    input: {id: 268, data: 0},
-    result: {id: 5, data: 0, count: 2},
-    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: 0.75}
-});
-sawmillManager.add({
-    input: {id: 271, data: 0},
-    result: {id: 5, data: 0, count: 1},
+    input: {id: VanillaBlockID.log2, data: 1},
+    result: {id: VanillaBlockID.planks, data: 5, count: 6},
     second: {id: ItemID.dustSaw, data: 0, count: 1}
 });
 
+//door -> planks
 sawmillManager.add({
-    input: {id: 290, data: 0},
-    result: {id: 5, data: 0, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
+    input: {id: VanillaBlockID.wooden_door},
+    result: {id: VanillaBlockID.planks, data: 0, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .5},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.spruce_door},
+    result: {id: VanillaBlockID.planks, data: 1, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .5},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.birch_door},
+    result: {id: VanillaBlockID.planks, data: 2, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .5},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.jungle_door},
+    result: {id: VanillaBlockID.planks, data: 3, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .5},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.acacia_door},
+    result: {id: VanillaBlockID.planks, data: 4, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .5},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.dark_oak_door},
+    result: {id: VanillaBlockID.planks, data: 5, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .5},
+    energy: 2000
+});
+
+//boat -> planks
+sawmillManager.add({
+    input: {id: VanillaItemID.boat},
+    result: {id: VanillaBlockID.planks, data: 0, count: 4},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaItemID.boat, data: 1},
+    result: {id: VanillaBlockID.planks, data: 1, count: 4},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaItemID.boat, data: 2},
+    result: {id: VanillaBlockID.planks, data: 2, count: 4},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaItemID.boat, data: 3},
+    result: {id: VanillaBlockID.planks, data: 3, count: 4},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaItemID.boat, data: 4},
+    result: {id: VanillaBlockID.planks, data: 4, count: 4},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaItemID.boat, data: 5},
+    result: {id: VanillaBlockID.planks, data: 5, count: 4},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+
+//stairs -> planks
+sawmillManager.add({
+    input: {id: VanillaBlockID.oak_stairs, count: 2},
+    result: {id: VanillaBlockID.planks, data: 0, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.spruce_stairs, count: 2},
+    result: {id: VanillaBlockID.planks, data: 1, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.birch_stairs, count: 2},
+    result: {id: VanillaBlockID.planks, data: 2, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.birch_stairs, count: 2},
+    result: {id: VanillaBlockID.planks, data: 2, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.jungle_stairs, count: 2},
+    result: {id: VanillaBlockID.planks, data: 3, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.acacia_stairs, count: 2},
+    result: {id: VanillaBlockID.planks, data: 4, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
+});
+sawmillManager.add({
+    input: {id: VanillaBlockID.dark_oak_stairs, count: 2},
+    result: {id: VanillaBlockID.planks, data: 5, count: 1},
+    second: {id: ItemID.dustSaw, data: 0, count: 1, chance: .25},
+    energy: 2000
 });
 
 sawmillManager.add({
-    input: {id: 270, data: 0},
-    result: {id: 5, data: 0, count: 1},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
+    input: {id: VanillaItemID.saddle},
+    result: {id: VanillaItemID.leather, data: 0, count: 3},
+    energy: 1500
 });
 
+//leather armor -> leather
 sawmillManager.add({
-    input: {id: 54, data: 0},
-    result: {id: 5, data: 0, count: 4},
-    second: {id: ItemID.dustSaw, data: 0, count: 1}
+    input: {id: VanillaItemID.leather_helmet},
+    result: {id: VanillaItemID.leather, data: 0, count: 2},
+    energy: 1500
 });
-
 sawmillManager.add({
-    input: {id: 103, data: 0},
-    result: {id: 360, data: 0, count: 9}
+    input: {id: VanillaItemID.leather_chestplate},
+    result: {id: VanillaItemID.leather, data: 0, count: 4},
+    energy: 1500
+});
+sawmillManager.add({
+    input: {id: VanillaItemID.leather_leggings},
+    result: {id: VanillaItemID.leather, data: 0, count: 3},
+    energy: 1500
+});
+sawmillManager.add({
+    input: {id: VanillaItemID.leather_boots},
+    result: {id: VanillaItemID.leather, data: 0, count: 2},
+    energy: 1500
 });
